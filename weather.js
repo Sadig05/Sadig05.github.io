865c6b54111d3a7b27617198cccefed9337ac75b
sadig05 <Sadiq.letifli70@gmail.com>
2022-12-04 02:36:33 +0400

    demo is ready

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 const API_KEY = "399c1caa155a987b83546a1c6c696e7e";
 
+//selecting html tags to access them
 let input_section = document.getElementById("input");
 let output_section = document.getElementById("output");
 let city_name = document.createElement("input");
@@ -12,15 +13,19 @@ submit_btn_city = document.createElement("button");
 submit_btn_city.textContent = "submit";
 submit_btn_city.setAttribute("onClick", "getCityName()");
 input_section.appendChild(submit_btn_city);
+output_section.style.display = "none";
 
+
+//gets city name from user and sends it to api
 function getCityName(){
     if (city_name.value.trim()){
        getByCityName(city_name.value);
     } else{
-        console.log("enter input");
+        alert("enter input");
     }
 }
 
+//selecting html tags to access them for searching based on coordinates
 let lat_input = document.createElement("input");
 let lon_input = document.createElement("input");
 lat_input.setAttribute("type", "text");
@@ -29,17 +34,21 @@ submit_btn_coor = document.createElement("button");
 submit_btn_coor.textContent = "submit";
 submit_btn_coor.setAttribute("onClick", "getLatLon()");
 
+//gets latitude and longitude from user and send them to api
 function getLatLon(){
     if(lat_input.value.trim() && lon_input.value.trim()){
         getData(lat_input.value, lon_input.value)
     } else{
-        console.log("wrong coordinates")
+        alert("wrong coordinates")
     }
 }
+
 
 let current_loc_button = document.createElement("button");
 current_loc_button.textContent = "My Location";
 current_loc_button.setAttribute("onClick", "getLocation()");
+
+//gets user's current location
 function getLocation(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -48,6 +57,11 @@ function getLocation(){
     }
 }
 
+function showPosition(position){
+    getData(position.coords.latitude,position.coords.longitude )
+}
+
+//gets the selected search method
 function getOption(){
     selected_option = document.getElementById("methods");
     output = selected_option.value;
@@ -55,13 +69,10 @@ function getOption(){
     return output;
 }
 
-function showPosition(position){
-    getData(position.coords.latitude,position.coords.longitude )
-}
-
-
+//shows the input field based on the selected search method
 function showInputMethod(){
     input_section.innerHTML = "";
+
     if(getOption() === 'city'){
         input_section.appendChild(city_label);
         input_section.appendChild(city_name);
@@ -82,28 +93,90 @@ function showInputMethod(){
     }
 }
 
+//selecting the output fields
+let city_name_output = document.getElementById("city-name");
+let country_name_output = document.getElementById("country-name");
+let weather_main_output = document.getElementById("weather-main");
+let weather_desc_output = document.getElementById("weather-desc");
+let temperature_output = document.getElementById("temperature");
+let humidity_output = document.getElementById("humidity");
+let pressure_output = document.getElementById("pressure");
+let wind_speed_output = document.getElementById("wind-speed");
+
+
+//displays the fetched data
+async function showData(data){
+    if(!data){
+        return
+    } else{
+        output_section.style.display = "block";
+       city_name_output.innerHTML = `city: ${data.name}`;
+       country_name_output.innerHTML = `Country: ${await getCountryName(data.sys.country)}`
+       weather_main_output.innerHTML = `weather: ${data.weather[0].main}`;
+       weather_desc_output.innerHTML = `weather description: ${data.weather[0].description}`;
+       temperature_output.innerHTML = `temperature: ${Math.floor(data.main.temp)}℃, minimum temperature: ${data.main.temp_min}℃, maximum temperature: ${data.main.temp_max}℃, feels like: ${data.main.feels_like}℃`;
+       humidity_output.innerHTML = `humidity: ${data.main.humidity}%`;
+        pressure_output.innerHTML = `pressure: ${data.main.pressure}`;
+        wind_speed_output.innerHTML = `wind speed: ${data.wind.speed}km/h, wind direction: ${findWindDirection(data.wind.deg)}`;
+    }
+}
+
+//based on the given degree decides the direction of a wind
+function findWindDirection(deg){
+    if(deg <= 90){
+        return "east"
+    } else if(deg <= 180){
+        return  "south"
+    }
+    else if(deg <= 270){
+        return "west"
+    }else{
+        return "north"
+    }
+}
+
+//fetching data from api by latitude and longitude;
 async function getData(lat, lon){
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`)
         const data = await response.json();
 
-        // showData(data);
+        showData(data);
         // return data
     }catch (e){
         console.error(e)
     }
 
 }
-
+// fetching data from api by city name;
 async function getByCityName(city){
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${API_KEY}`);
         const data = await response.json();
         console.log(data)
-        // showData(data)
+        showData(data)
         // return data
     } catch (e){
         console.error(e)
     }
 
 }
+
+//based on the given country code fetches the full name of the country
+async function getCountryName(code){
+    try {
+        const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+        const data = await response.json();
+        console.log(data[0].name.common)
+        return data[0].name.common;
+    }catch (e){
+        console.error(e);
+    }
+}
+
+
+
+
+//add html link to navbar;
+//add styling
+//correct temperature based on the metric unit;
